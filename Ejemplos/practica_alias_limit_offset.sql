7001d1a8c8a75164b5a287c139ae72992e8d6836
bertichelucas <bertichelucas@gmail.com>
2022-06-05 18:54:51 -0300

    Practicas clase 11 con movies-db

---
+++ Ejemplos/practica_alias_limit_offset.sql
@@ -0,0 +1,36 @@
+/*** ALIAS LIMIT Y OFFSET ***/
+
+#Mostrar el título de todas las series y usar alias para que el nombre del campo
+#este en español.
+
+SELECT title AS titulo FROM series;
+
+/*
+Traer el título de las películas con el rating mayor a 3, con más de 1 premio y con
+fecha de lanzamiento entre el año ‘1988-01-01’ al ‘2009-12-31’. Ordenar los
+resultados por rating descendentemente.
+*/
+
+SELECT title AS titulo, rating AS calificacion FROM movies
+WHERE rating > 3 AND awards > 1 AND release_date BETWEEN "1988-01-01" AND "2009-12-31"
+ORDER BY rating DESC;
+
+#Traer el top 3 a partir del registro 10 de la consulta anterior.
+
+SELECT title AS titulo, rating AS calificacion FROM movies
+WHERE rating > 3 AND awards > 1 AND release_date BETWEEN "1988-01-01" AND "2009-12-31"
+ORDER BY rating DESC
+LIMIT 3;
+
+#¿Cuáles son los 3 peores episodios teniendo en cuenta su rating?
+
+SELECT title, number, rating FROM episodes
+ORDER BY rating
+LIMIT 3;
+
+#Obtener el listado de todos los actores. Quitar las columnas de fechas y película
+#favorita, además traducir los nombres de las columnas.
+
+SELECT first_name AS Nombre, last_name AS Apellido, rating AS Calificacion FROM actors;
+
+
---
+++ Ejemplos/practica_select_where_orderBy_between_like.sql
@@ -0,0 +1,44 @@
+/*** SELECT ***/
+
+SELECT * FROM movies;
+
+SELECT first_name, last_name, rating FROM actors;
+
+SELECT title FROM series;
+
+/*** WHERE Y ORDER BY ***/
+
+#Mostrar el nombre y apellido de los actores cuyo rating sea mayor a 7,5.
+
+SELECT first_name, last_name from actors
+WHERE rating > 7.5;
+
+#Mostrar el título de las películas, el rating y los premios de las películas con
+#un rating mayor a 7,5 y con más de dos premios.
+
+SELECT title, rating, awards FROM movies
+WHERE rating > 7.5 AND awards > 2;
+
+#Mostrar el título de las películas y el rating ordenadas por rating en forma
+#ascendente.
+
+SELECT title, rating FROM movies
+ORDER BY rating;
+
+/*** BETWEEN Y LIKE ***/
+
+#Mostrar el título y rating de todas las películas cuyo título incluya Toy Story.
+
+SELECT title, rating FROM movies
+WHERE title LIKE "%Toy Story%";
+
+#Mostrar a todos los actores cuyos nombres empiecen con Sam.
+
+SELECT * FROM actors
+WHERE first_name LIKE "Sam%";
+
+#Mostrar el título de las películas que salieron entre el ‘2004-01-01’ y
+#‘2008-12-31’.
+
+SELECT title FROM movies
+WHERE release_date BETWEEN "2004-01-01" AND "2008-12-31";
